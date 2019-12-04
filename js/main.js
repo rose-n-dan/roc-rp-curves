25de6c84c8b63060f0b2238d2388fefc5c674a95
Daniel <danielm1405@gmail.com>
2019-12-04 16:34:22 +0100

    some tests

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-function Plotter() {
+function plotter() {
   TESTER = document.getElementById('tester');
 
   Plotly.plot( TESTER, [{
     x: [1, 2, 3, 4, 5],
     y: [1, 2, 4, 8, 16] }], {
     margin: { t: 0 } }, {showSendToCloud:true} );
+}
+
+function saveParams() {
+  console.log("I'm inside saveParams.");
+  let param1 = document.getElementById("param1").value;
+  let param2 = document.getElementById("param2").value;
+  console.log("Param1 = " + param1);
+  console.log("param2 = " + param2);
+}
+
+function addListeners() {
+  document.getElementById("save_button").addEventListener("click", saveParams, false);
 }
