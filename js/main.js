28891bdb20d2adad108170cbc85ee186945b26a8
KlonKlona <rosjan.rosa@gmail.com>
2019-12-10 07:36:08 +0100

    Add data processing and plots

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 function plotter() {
-  let TESTER = document.getElementById('tester');
-
   // collect values and test results
   let data_table = document.getElementById('data_table');
   let values = [], test_results = [];
@@ -19,11 +17,116 @@ function plotter() {
   }
   console.log("Thresholds: " + thresholds);
 
-  // TODO: insert proper data
-  Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-    margin: { t: 0 } }, {showSendToCloud:true} );
+  // parse data as error matrix rows
+  let processedRows = processRows(values, test_results, thresholds);
+  for (let i = 0; i < processedRows.length; i++) {
+    let matrix = document.getElementById('error_matrix');
+    let len = matrix.rows.length;
+    let new_row = matrix.insertRow(len);
+    let threshold_cell = new_row.insertCell(0);
+    let TP_cell = new_row.insertCell(1);
+    let FP_cell = new_row.insertCell(2);
+    let TN_cell = new_row.insertCell(3);
+    let FN_cell = new_row.insertCell(4);
+    threshold_cell.innerHTML = processedRows[i].threshold;
+    TP_cell.innerHTML = processedRows[i].TP;
+    FP_cell.innerHTML = processedRows[i].FP;
+    TN_cell.innerHTML = processedRows[i].TN;
+    FN_cell.innerHTML = processedRows[i].FN;
+  }
+
+  // prepare data for ROC curve
+  let TPR = [];
+  let FPR = [];
+  let PPV = [];
+  for (let i = 0; i < processedRows.length; i++) {
+    TPR.push(parseFloat(processedRows[i].recall));
+    FPR.push(parseFloat(1 - processedRows[i].specifity));
+    PPV.push(parseFloat(processedRows[i].precision));
+
+  }
+
+  // draw ROC curve
+  let roc = {
+    x: FPR,
+    y: TPR,
+    type: 'scatter'
+  };
+  let data_roc = [roc];
+  let layout_roc = {
+    title: {
+      text:'Receiver Operating Characteristic Curve',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'False Positive Probability',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'True Positive Probability',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+
+  Plotly.newPlot('rocPlot', data_roc, layout_roc, {showSendToCloud: true});
+
+  // draw RP curve
+  let rp = {
+    x: TPR,
+    y: PPV,
+    type: 'scatter'
+  };
+  let data_rp = [rp];
+  let layout_rp = {
+    title: {
+      text:'Recall Precision Curve',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'True Positive Probability',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Positive Predictive Probability',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+
+  Plotly.newPlot('rcPlot', data_rp, layout_rp, {showSendToCloud: true});
 }
 
 function addRow() {
@@ -35,15 +138,42 @@ function addRow() {
   let test_result_cell = new_row.insertCell(1);
   value_cell.innerHTML = document.getElementById("value").value;
   test_result_cell.innerHTML = document.getElementById("test_result").value;
+  document.getElementById("resultForm").reset();
 }
 
 function addThreshold() {
-  console.log("Adding new threshold.");
-  let table = document.getElementById('thresholds_table');
-  let len = table.rows.length;
-  let new_row = table.insertRow(len);
-  let threshold_cell = new_row.insertCell(0);
-  threshold_cell.innerHTML = document.getElementById("threshold").value;
+  let threshold = document.getElementById('threshold').value;
+  if(validateThreshold(threshold) == true) {
+    console.log("Adding new threshold.");
+    let table = document.getElementById('thresholds_table');
+    let len = table.rows.length;
+    let new_row = table.insertRow(len);
+    let threshold_cell = new_row.insertCell(0);
+    threshold_cell.innerHTML = document.getElementById("threshold").value;
+  }
+  document.getElementById("thresholdForm").reset();
+}
+
+function validateThreshold(input) {
+  let threshold = input;
+  let thresholdREGEX = /^0[.][0-9]+$|^1$|^0$/;
+  let thresholdResult = thresholdREGEX.test(threshold);
+  if(thresholdResult == false) {
+    alert('Please enter a valid threshold');
+    return false;
+  }
+  return true;
+}
+
+function validateResult(input) {
+  let threshold = input;
+  let thresholdREGEX = /^0[.][0-9]+$|^1$|^0$/;
+  let thresholdResult = thresholdREGEX.test(threshold);
+  if(thresholdResult == false) {
+    alert('Please enter a valid test result');
+    return false;
+  }
+  return true;
 }
 
 function addListeners() {
